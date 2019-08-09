ebe7e973bf6e6b8967b70b48fe649e3f34cfcf6f
Hassani <hmasudi@gmail.com>
2019-08-09 13:23:27 +0200

    initial

--- accounting/settings.py
+++ accounting/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$fmvwy77v!dmc1umwugq)_&p8*os!^^ec56u&*)=-$#!%i_e2n'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bookkeeper',
+    'crispy_forms',
+    'import_export',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'accounting.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'bookkeeper/template')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
